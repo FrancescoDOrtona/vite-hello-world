65104f444a013306b5c68475257dc4e61bd64864
FrancescoDOrtona <francescod.97@hotmail.it>
2023-11-08 02:07:00 +0900

    Add: Ex hello vite

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+export default{
+ data() {
+  return {
+    message: 'La mia prima app con vite!'
+  }
+ },
+}
+</script>
+
+<template>
+  <div> 
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
